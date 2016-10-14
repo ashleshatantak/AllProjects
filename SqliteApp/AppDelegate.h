c80d2d92812fdfcb30149b5d834d9ac6a23a9c79
MadhuriJagtap <madhuri@felix-its.com>
2016-10-14 15:06:25 +0530

    Initial Commit

---
+++ SqliteApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SqliteApp
+//
+//  Created by Felix-Madhuri on 14/10/16.
+//  Copyright © 2016 Felix-Madhuri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SqliteApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SqliteApp
+//
+//  Created by Felix-Madhuri on 14/10/16.
+//  Copyright © 2016 Felix-Madhuri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
