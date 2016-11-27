174f4dc13d4a66d1e1366f9bb3c00336101cf39c
Ashlesha <ashlesha@ashleshas-mac.local>
2016-11-27 22:19:27 +0530

    Initial Commit

---
+++ Tic Tac Toe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tic Tac Toe
+//
+//  Created by Ashlesha on 27/11/16.
+//  Copyright © 2016 Ashlesha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
